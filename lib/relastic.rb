8ef14437ea7e38634d6cd1153d19c3ea890fb672
Thomas Tych <thomas.tych@gmail.com>
2021-12-01 14:53:13 +0100

    init relastic

---
+++ lib/relastic.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "relastic/version"
+
+module Relastic
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/relastic_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Relastic do
+  it "has a version number" do
+    expect(Relastic::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
